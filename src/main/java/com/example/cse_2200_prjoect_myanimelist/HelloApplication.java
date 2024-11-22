7009f527b35809db5fbe4f6dd1702f7553bb2efe
rahman-arifurr <arifurr946@gmail.com>
2024-11-22 16:10:28 +0600

    made some login page ish thingy

--- src/main/java/com/example/cse_2200_prjoect_myanimelist/HelloApplication.java
+++ src/main/java/com/example/cse_2200_prjoect_myanimelist/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.cse_2200_prjoect_myanimelist;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("MyAnimeList");
+        Image icon = new Image(getClass().getResourceAsStream("titleicon.png")); // Adjust path if needed
+        stage.getIcons().add(icon);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
